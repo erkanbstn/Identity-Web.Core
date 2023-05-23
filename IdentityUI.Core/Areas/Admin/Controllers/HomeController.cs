46ecc189f60d9a04f2533b0117beb5ed51182b01
Erkan Bostan <profesyonel59@gmail.com>
2023-05-23 21:16:54 +0300

    New, Edit, Remove Role Transactions Completed.

--- IdentityUI.Core/Areas/Admin/Controllers/HomeController.cs
+++ IdentityUI.Core/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityUI.Core.Areas.Admin.Models;
+using IdentityUI.Core.Extensions;
 using IdentityUI.Core.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,16 +11,13 @@ namespace IdentityUI.Core.Areas.Admin.Controllers
     public class HomeController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
-
-        public HomeController(UserManager<AppUser> userManager)
+        private readonly RoleManager<AppRole> _roleManager;
+        public HomeController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
         public async Task<IActionResult> UserList()
         {
             var userList = await _userManager.Users.ToListAsync();
@@ -30,6 +28,61 @@ namespace IdentityUI.Core.Areas.Admin.Controllers
                 Name = b.UserName
             }).ToList();
             return View(userViewModelList);
+        }
+        public async Task<IActionResult> RoleList()
+        {
+            var roles = await _roleManager.Roles.Select(b => new RoleViewModel()
+            {
+                Id = b.Id,
+                Name = b.Name
+            }).ToListAsync();
+            return View(roles);
+        }
+        public IActionResult NewRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> NewRole(RoleViewModel roleViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var result = await _roleManager.CreateAsync(new AppRole()
+            {
+                Name = roleViewModel.Name,
+            });
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelErrorList(result.Errors);
+                return View();
+            }
+            return RedirectToAction(nameof(HomeController.RoleList));
+        }
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            return View(new RoleViewModel() { Id = role.Id, Name = role.Name });
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(RoleViewModel roleViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var role = await _roleManager.FindByIdAsync(roleViewModel.Id);
+            role.Name=roleViewModel.Name;
+            await _roleManager.UpdateAsync(role);
+            TempData["Success"] = "Rol Başarıyla Güncellendi";
+            return View();
+        }
+        public async Task<IActionResult> RemoveRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            await _roleManager.DeleteAsync(role);
+            return RedirectToAction(nameof(HomeController.RoleList));
         }
     }
 }
---
+++ IdentityUI.Core/Areas/Admin/Models/RoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityUI.Core.Areas.Admin.Models
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Rol Alanı Boş Bırakılamaz")]
+        public string Name { get; set; }
+    }
+}
