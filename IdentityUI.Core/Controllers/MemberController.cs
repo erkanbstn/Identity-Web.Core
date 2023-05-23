404d4dec4feff6eb9cfae01ee9ba863b2a87d043
Erkan Bostan <profesyonel59@gmail.com>
2023-05-23 16:12:01 +0300

    Password Changes Transactions Completed.

--- IdentityUI.Core/Controllers/MemberController.cs
+++ IdentityUI.Core/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
-﻿using IdentityUI.Core.Models;
+﻿using IdentityUI.Core.Extensions;
+using IdentityUI.Core.Models;
 using IdentityUI.Core.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -10,10 +11,12 @@ namespace IdentityUI.Core.Controllers
     public class MemberController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public MemberController(UserManager<AppUser> userManager)
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public async Task<IActionResult> Index()
@@ -25,6 +28,36 @@ namespace IdentityUI.Core.Controllers
         public IActionResult ReturnTest()
         {
             return View();
+        }
+        public IActionResult PasswordChange()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> PasswordChange(PasswordChangeViewModel passwordChangeViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var currentUser = await _userManager.FindByNameAsync(User.Identity.Name);
+            var checkOldPassword = await _userManager.CheckPasswordAsync(currentUser, passwordChangeViewModel.OldPassword);
+            if (!checkOldPassword)
+            {
+                ModelState.AddModelError(string.Empty, "Eski Şifreniz Yanlıştır.");
+                return View();
+            }
+            var result = await _userManager.ChangePasswordAsync(currentUser, passwordChangeViewModel.OldPassword,passwordChangeViewModel.NewPassword);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelErrorList(result.Errors.Select(b=>b.Description).ToList());
+                return View();
+            }
+            await _signInManager.SignOutAsync();
+            await _signInManager.PasswordSignInAsync(currentUser,passwordChangeViewModel.NewPassword,true,false);
+            await _userManager.UpdateSecurityStampAsync(currentUser);
+            TempData["Success"] = "Parolanız Başarıyla Değiştirildi";
+            return RedirectToAction("PasswordChange");
         }
     }
 }
---
+++ IdentityUI.Core/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityUI.Core.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [Required(ErrorMessage = "Eski Parola Alanı Boş Bırakılamaz")]
+        [MinLength(6,ErrorMessage = "Şifreniz En Az 6 Karakter Olabilir.")]
+        public string OldPassword { get; set; }
+        [Required(ErrorMessage = "Yeni Parola Alanı Boş Bırakılamaz")]
+        [MinLength(6, ErrorMessage = "Şifreniz En Az 6 Karakter Olabilir.")]
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Parola Tekrar Alanı Boş Bırakılamaz")]
+        [MinLength(6, ErrorMessage = "Şifreniz En Az 6 Karakter Olabilir.")]
+        [Compare(nameof(NewPassword), ErrorMessage = "Şifreler Aynı Olmalıdır")]
+        public string ConfirmPassword { get; set; }
+    }
+}
